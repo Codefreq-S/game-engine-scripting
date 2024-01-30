004b48892dbf4615ec76e4378a1686af84f23b3c
Codefreq-S <zack@codefreqs.net>
2024-01-29 20:26:12 -0600

    Week 2 In-Class

---
+++ Assets/Week-2/Scripts/Character.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    bool didUpdate = false;
+    bool didLateUpdate = false;
+
+    public static int health = 100;
+    void Awake()
+    {
+        instance = this;
+        //Debug.Log(Character.health);
+        Debug.Log("character is awake:");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I AM STARTING");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(didUpdate)
+        {
+            Debug.Log("I AM UPDATED");
+            Debug.LogWarning("STOP OR YOU WILL BE ARRESTED");
+            Debug.LogError("TGIS ERROR SHOULD NOT HAPPEN EMAIL ccintron@colum.edu");
+            didUpdate = true;
+        }
+    }
+    void LateUpdate()
+    {
+        if(didLateUpdate)
+        {
+            Debug.Log("Late Update");
+            didLateUpdate = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        Debug.Log("I am enabled");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("I am disabled");
+    }
+}
